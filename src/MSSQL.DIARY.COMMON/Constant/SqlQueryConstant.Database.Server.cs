19f1a0888ea90e4e76c83617442d261a6c2f0b96
Lakshaman Rokade <lakshaman.rokade@sagitec.com>
2021-04-03 13:11:24 +0530

    Code moving into single file

--- src/MSSQL.DIARY.COMMON/Constant/SqlQueryConstant.Database.Server.cs
+++ src/MSSQL.DIARY.COMMON/Constant/SqlQueryConstant.Database.Server.cs
@@ -9,35 +9,27 @@
 
         public static readonly string GetTables = @"[sys].[sp_tables]";
 
-        public static readonly string GetTableColumns =
-            @"SELECT  c.name AS column_name FROM sys.tables AS t INNER JOIN sys.columns c ON t.OBJECT_ID = c.OBJECT_ID WHERE t.name LIKE '%@tableName%' ";
+        public static readonly string GetTableColumns = @"SELECT  c.name AS column_name FROM sys.tables AS t INNER JOIN sys.columns c ON t.OBJECT_ID = c.OBJECT_ID WHERE t.name LIKE '%@tableName%' ";
 
-        public static readonly string GetStoreProcedures =
-            @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'STOREPROC' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
+        public static readonly string GetStoreProcedures = @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'STOREPROC' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
                                                               WHERE O.TYPE='P'";
 
-        public static readonly string GetTigger =
-            @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'Tiggers' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
+        public static readonly string GetTigger = @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'Tiggers' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
                                                               WHERE O.TYPE='TR'";
 
-        public static readonly string GetScalarFunctions =
-            @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'SQL_SCALAR_FUNCTION' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
+        public static readonly string GetScalarFunctions = @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'SQL_SCALAR_FUNCTION' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
                                                               WHERE O.TYPE='FN'";
 
-        public static readonly string GetTableValueFunctions =
-            @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'SQL_TABLE_VALUED_FUNCTION' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
+        public static readonly string GetTableValueFunctions = @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'SQL_TABLE_VALUED_FUNCTION' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
                                                               WHERE O.TYPE='TF'";
 
-        public static readonly string GetAggregateFunctions =
-            @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'SQL_AGGREGATE_FUNCTION' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
+        public static readonly string GetAggregateFunctions = @"SELECT ((SCHEMA_NAME(O.SCHEMA_ID) )+'.'+ [NAME])AS 'SQL_AGGREGATE_FUNCTION' FROM SYS.SQL_MODULES M INNER JOIN SYS.OBJECTS O ON M.OBJECT_ID=O.OBJECT_ID
                                                               WHERE O.TYPE='IF'";
 
-        public static readonly string GetUserDefinedDataType =
-            @"SELECT (SCHEMA_NAME(SCHEMA_ID) +'.'+[NAME])AS 'USERDEFINEDDATATYPE' FROM SYS.TYPES
+        public static readonly string GetUserDefinedDataType = @"SELECT (SCHEMA_NAME(SCHEMA_ID) +'.'+[NAME])AS 'USERDEFINEDDATATYPE' FROM SYS.TYPES
                                                                WHERE IS_USER_DEFINED = 1";
 
-        public static readonly string GetXmlSchemas =
-            @"SELECT DISTINCT (SCHEMA_NAME(SCHEMA_ID)+'.'+XSC.NAME) AS 'NAME' FROM    SYS.XML_SCHEMA_COLLECTIONS XSC JOIN SYS.XML_SCHEMA_NAMESPACES XSN  ON (XSC.XML_COLLECTION_ID = XSN.XML_COLLECTION_ID)";
+        public static readonly string GetXmlSchemas = @"SELECT DISTINCT (SCHEMA_NAME(SCHEMA_ID)+'.'+XSC.NAME) AS 'NAME' FROM    SYS.XML_SCHEMA_COLLECTIONS XSC JOIN SYS.XML_SCHEMA_NAMESPACES XSN  ON (XSC.XML_COLLECTION_ID = XSN.XML_COLLECTION_ID)";
 
         public static readonly string[] GetServerProperties =
         {
@@ -66,7 +58,6 @@
                    END AS IsClustered"
         };
 
-        public static readonly string GetAdvancedServerSettings =
-            @"select  value_name as Property , CAST(value_data as VARCHAR(1000))as   'Value'   from sys.dm_server_registry";
+        public static readonly string GetAdvancedServerSettings = @"select  value_name as Property , CAST(value_data as VARCHAR(1000))as   'Value'   from sys.dm_server_registry";
     }
 }
